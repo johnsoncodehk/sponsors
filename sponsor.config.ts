6961e7a05e9cab3507bca176723860c446c7384a
Johnson Chu <johnsoncodehk@gmail.com>
2024-04-07 20:26:27 +0800

    Update sponsor.config.ts

--- sponsor.config.ts
+++ sponsor.config.ts
@@ -1,22 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { defineConfig, presets, Sponsorship } from 'sponsorkit'
-
-export function onSponsorsFetched(sponsors: Sponsorship[]) {
-  defineTierForSponsorInPast(sponsors, 'yaegassy', 100, new Date(2022, 6, 15, 0, 0, 0, 0).valueOf());
-  defineTierForSponsorInPast(sponsors, 'programmierbar', 1000, new Date(2022, 5, 20, 0, 0, 0, 0).valueOf());
-
-  for (const sponsor of sponsors) {
-    if (sponsor.sponsor.login === 'marvin-robot') {
-      sponsor.sponsor.login = 'PrefectHQ';
-      sponsor.sponsor.name = 'Prefect';
-      sponsor.sponsor.type = 'Organization';
-      sponsor.sponsor.avatarUrl = 'https://avatars.githubusercontent.com/u/39270919?s=200&v=4';
-    }
-  }
-}
+import { defineConfig, presets } from 'sponsorkit'
 
 export default defineConfig({
-  onSponsorsFetched,
   tiers: [
     {
       title: 'Sponsors',
@@ -36,14 +21,3 @@ export default defineConfig({
     },
   ],
 })
-
-function defineTierForSponsorInPast(sponsors: Sponsorship[], username: string, tier: number, expire: number) {
-
-  if (Date.now() > expire)
-    return;
-
-  let sponsor = sponsors.find(sponsor => sponsor.sponsor.login === username);
-  if (sponsor) {
-    sponsor.monthlyDollars = Math.max(sponsor.monthlyDollars, tier);
-  }
-}
