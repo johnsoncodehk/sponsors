6adcc630b2d91e39af290cea66d467ba4b804906
Johnson Chu <johnsoncodehk@gmail.com>
2025-06-27 08:49:32 +0800

    Update sponsor.config.ts

--- sponsor.config.ts
+++ sponsor.config.ts
@@ -1,24 +1,5 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { defineConfig, presets } from 'sponsorkit'
+import { defineConfig } from 'sponsorkit'
 
 export default defineConfig({
   renderer: 'circles',
-  tiers: [
-    {
-      title: 'Sponsors',
-      compose(composer, sponsors) {
-        composer.addTitle('Sponsors');
-        composer.addSponsorGrid(sponsors.filter(sponsor => sponsor.monthlyDollars >= 20), presets.medium);
-        composer.addSponsorGrid(sponsors.filter(sponsor => sponsor.monthlyDollars >= 8 && sponsor.monthlyDollars < 20), presets.base);
-        composer.addSponsorGrid(sponsors.filter(sponsor => sponsor.monthlyDollars < 8), presets.small);
-        composer.addSpan(35);
-        composer.addText('Thank you 💚');
-      },
-    },
-    {
-      title: 'Silver/Company Sponsors',
-      monthlyDollars: 100,
-      preset: presets.large,
-    },
-  ],
 })
